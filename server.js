9047020999abe47798b4761420d76aa7b1f68911
bproeckl <84336480+bproeckl@users.noreply.github.com>
2021-12-19 12:34:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const port = 3000
+const port = process.env.PORT || 3000
 const User = require('./models/user')
 const bcrypt = require('bcrypt')
 
